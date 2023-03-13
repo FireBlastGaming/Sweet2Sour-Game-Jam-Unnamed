b5953549a10ad8628558fd3567b19238be0ae091
FireBlastGaming <iboo.abdulali@gmail.com>
2023-03-14 00:13:16 +0400

    Movement
    Setting up Movement

--- Sweet2Sour Game Jam/Assets/Scripts/Player.cs
+++ Sweet2Sour Game Jam/Assets/Scripts/Player.cs
@@ -4,32 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float x;
-    private float y;
+
+    public float speed = 1.0f;
 
     void Update()
     {
-        x = transform.position.x;
-        y = transform.position.y;
 
-        if (Input.GetKeyDown(KeyCode.A))
+       speed -= Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector2(x - 1, y);
+            if (speed < 0)
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+            
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.position = new Vector2(x + 1, y);
+            if (speed < 0)
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position = new Vector2(x, y + 1);
+            if (speed < 0)
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
-            transform.position = new Vector2(x, y - 1);
+            if (speed < 0)
+            {
+                transform.position += new Vector3(0, -1, 0);
+            }
         }
     }
 }
