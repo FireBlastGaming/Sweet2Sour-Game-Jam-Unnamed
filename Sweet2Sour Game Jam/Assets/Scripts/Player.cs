ed24276e4169ff4b3289e53c2200b8b7836b3baa
FireBlastGaming <iboo.abdulali@gmail.com>
2023-03-15 19:48:08 +0400

    Basic Movement != Timer
    Without timer

--- Sweet2Sour Game Jam/Assets/Scripts/Player.cs
+++ Sweet2Sour Game Jam/Assets/Scripts/Player.cs
@@ -5,44 +5,33 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float speed = 1.0f;
+    
 
     void Update()
     {
 
-       speed -= Time.deltaTime;
-
         if (Input.GetKey(KeyCode.A))
         {
-            if (speed < 0)
-            {
                 transform.position += new Vector3(-1, 0, 0);
-            }
             
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            if (speed < 0)
-            {
                 transform.position += new Vector3(1, 0, 0);
-            }
+            
         }
 
         if (Input.GetKey(KeyCode.W))
         {
-            if (speed < 0)
-            {
                 transform.position += new Vector3(0, 1, 0);
-            }
+            
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            if (speed < 0)
-            {
                 transform.position += new Vector3(0, -1, 0);
-            }
+            
         }
     }
 }
