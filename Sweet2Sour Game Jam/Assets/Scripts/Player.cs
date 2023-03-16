d99c5f0da7037565303da7f2744be49eb5296df4
FireBlastGaming <iboo.abdulali@gmail.com>
2023-03-16 17:17:33 +0400

    Movement script with timer
    The timer is finished but it still requires tweaking.
    I still need to make it so the buffer resets to 0 every time I let go of the key.

--- Sweet2Sour Game Jam/Assets/Scripts/Player.cs
+++ Sweet2Sour Game Jam/Assets/Scripts/Player.cs
@@ -4,34 +4,75 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public float buffer;
     
 
     void Update()
     {
-
+        //The code inside is only run when the key "A" is clicked 
         if (Input.GetKey(KeyCode.A))
         {
+            //This if statement counts down the buffer. When the buffer is 0 then it moves the player and sets the buffer to 0.5s
+            if (buffer > 0)
+            {
+                buffer -= Time.deltaTime;
+            } 
+            else
+            {
                 transform.position += new Vector3(-1, 0, 0);
+                buffer = 0.55f;
+                    
+            }
             
         }
-
+        //The code inside is only run when the key "D" is clicked 
         if (Input.GetKey(KeyCode.D))
         {
+            //This if statement counts down the buffer. When the buffer is 0 then it moves the player and sets the buffer to 0.5s
+            if (buffer > 0)
+            {
+                buffer -= Time.deltaTime;
+            }
+            else
+            {
                 transform.position += new Vector3(1, 0, 0);
-            
-        }
+                buffer = 0.55f;
+            }
 
+        }
+        //The code inside is only run when the key "W" is clicked 
         if (Input.GetKey(KeyCode.W))
         {
+            //This if statement counts down the buffer. When the buffer is 0 then it moves the player and sets the buffer to 0.5s
+            if (buffer > 0)
+            {
+                buffer -= Time.deltaTime;
+            }
+            else
+            {
                 transform.position += new Vector3(0, 1, 0);
-            
-        }
+                buffer = 0.55f;
 
+            }
+            
+
+        }
+        //The code inside is only run when the key "S" is clicked 
         if (Input.GetKey(KeyCode.S))
         {
+            //This if statement counts down the buffer. When the buffer is 0 then it moves the player and sets the buffer to 0.5s
+            if (buffer > 0)
+            {
+                buffer -= Time.deltaTime;
+            }
+            else
+            {
                 transform.position += new Vector3(0, -1, 0);
-            
+                buffer = 0.55f;
+                    
+            }
+        
+
         }
     }
 }
